2fb759ee168d383a72ff1646f0ca5d3611eba064
David A <damanschool25@gmail.com>
2025-10-24 23:09:11 -0500

    testing2

--- bookbuddy-backend/bookbuddy-springboot/src/main/java/com/bookbuddy/controller/UserController.java
+++ bookbuddy-backend/bookbuddy-springboot/src/main/java/com/bookbuddy/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+// David Aiyeyemi
 @RestController
 @RequestMapping("/users")
 @CrossOrigin(origins = "http://localhost:5173") // your frontend URL
